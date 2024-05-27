3c870884d0887b7e605590add0f54f1b12974fb9
Yolisa Mzolisa <ypmzolisa@gmail.com>
2024-05-27 16:06:03 +0200

    i have seeded the database, gem install

--- db/migrate/20240527134334_create_items.rb
+++ db/migrate/20240527134334_create_items.rb
@@ -2,10 +2,10 @@ class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :category 
+      t.string :category
       t.text :description
       t.integer :rating
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
