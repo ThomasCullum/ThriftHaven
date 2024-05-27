6f1249f15231f7288a46c9d67fe8616bdde6fe7b
Thomas Cullum <thomascullum05@gmail.com>
2024-05-27 15:23:21 +0200

    creted new and create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.create(item_params)
+    redirect_to item_path(@item)
+  end
 end
--- db/migrate/20240527125910_create_items.rb
+++ db/migrate/20240527125910_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :category
+      t.string :category 
       t.text :description
       t.integer :rating
       t.references :user_id, null: false, foreign_key: true
